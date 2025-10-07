80c58e9c5774c363448ec52b350df441b8531352
verystupidowl <amaklov2002@gmail.com>
2025-10-07 14:45:33 +0400

    healthcheck

--- src/main/java/ru/tggc/capybaratelegrambot/controller/HealthCheckController.java
+++ src/main/java/ru/tggc/capybaratelegrambot/controller/HealthCheckController.java
@@ -1,15 +1,18 @@
 package ru.tggc.capybaratelegrambot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/health")
     public ResponseEntity<String> health() {
+        log.info("health checked");
         return ResponseEntity.status(HttpStatus.OK).body("ok");
     }
 }
