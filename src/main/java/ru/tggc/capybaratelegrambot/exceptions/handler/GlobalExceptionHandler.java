9892366fe56bd770d4ec6ce8f6df9537554ead67
verystupidowl <amaklov2002@gmail.com>
2025-12-02 16:47:45 +0400

    fix

--- src/main/java/ru/tggc/capybaratelegrambot/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/tggc/capybaratelegrambot/exceptions/handler/GlobalExceptionHandler.java
@@ -1,10 +1,9 @@
-package ru.tggc.capybaratelegrambot.handler;
+package ru.tggc.capybaratelegrambot.exceptions.handler;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,9 +14,8 @@ public class GlobalExceptionHandler {
     public final TelegramBot telegramBot;
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> handleException(Exception e) {
+    public void handleException(Exception e) {
         telegramBot.execute(new SendMessage(428873987, e.getMessage()));
         log.error(e.getMessage(), e);
-        return ResponseEntity.ok().build();
     }
 }
