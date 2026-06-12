52010d20d88a470dce9dab95708575c63e093e15
verystupidowl <amaklov2002@gmail.com>
2026-06-12 22:28:51 +0400

    fix 25 java dockerfile

--- src/main/java/ru/tggc/capybaratelegrambot/config/TelegramConfig.java
+++ src/main/java/ru/tggc/capybaratelegrambot/config/TelegramConfig.java
@@ -1,6 +1,7 @@
 package ru.tggc.capybaratelegrambot.config;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SetWebhook;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +16,12 @@ public class TelegramConfig {
 
     @Bean
     public TelegramBot telegramBot(@Value("${bot.token}") String botToken,
-                                   @Value("${bot.webhook_url}") String webhookUrl) {
+                                   @Value("${bot.webhook_url}") String webhookUrl,
+                                   @Value("${bot.admin-id}") String adminId) {
         TelegramBot bot = new TelegramBot(botToken);
         var response = bot.execute(new SetWebhook().url(webhookUrl));
         log.info("Webhook info {}", response);
+        bot.execute(new SendMessage(adminId, "Webhook has been set " + response));
         return bot;
     }
 }
--- src/main/java/ru/tggc/capybaratelegrambot/listener/Listener.java
+++
@@ -1,8 +0,0 @@
-package ru.tggc.capybaratelegrambot.listener;
-
-import java.util.function.Consumer;
-
-public interface Listener<R> {
-
-    void listen(Consumer<R> response);
-}
